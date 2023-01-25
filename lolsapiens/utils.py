d43c75e4fad9234c8dee17d8f09972344ce65132
Juan Sebastian Peña <jspenaq@unal.edu.co>
2023-01-25 15:54:50 -0500

    refactor code, lolsapiens package

---
+++ lolsapiens/utils.py
@@ -0,0 +1,51 @@
+import argparse
+from os import makedirs
+from os.path import exists
+
+
+def create_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--champion-name", help="Champion name")
+    parser.add_argument(
+        "-l",
+        "--lane",
+        choices=["top", "jungle", "middle", "bottom", "support"],
+        help="Lane",
+    )
+    parser.add_argument(
+        "-t",
+        "--tier",
+        default="platinum_plus",
+        choices=["gold_plus", "platinum_plus", "diamond_plus", "all", "1trick"],
+        help="Tier data",
+    )
+    parser.add_argument(
+        "-k",
+        "--keystone-name",
+        # choices=[],
+        help="Keystone name",
+    )
+    parser.add_argument(
+        "--import",
+        action="store_true",
+        help="Import automatically to League of Legends folder",
+        dest="IMPORT"
+    )
+
+    return parser
+
+
+def setup_folders() -> bool:
+    try:
+        if not exists("data"):
+            makedirs("data")
+
+        if not exists("Champions"):
+            makedirs("Champions")
+
+    except Exception as e:
+        print("An exception occurred:")
+        print(e)
+        return False
+
+    return True
---
+++ tests/test_basic.py
@@ -0,0 +1,4 @@
+import lolsapiens.lolsapiens as ls
+
+def test_get_languages():
+    assert type(ls.get_languages()) is list
