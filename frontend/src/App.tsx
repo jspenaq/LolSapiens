45991b15f45592fa3fd9497eb52020c3b47a4bf8
Andres Gonzalez <adgonzalezo@unal.edu.co>
2023-02-06 12:26:01 -0500

    scss added :D

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef, useLayoutEffect } from 'react'
+import { useState, useEffect, useRef, useLayoutEffect } from "react";
 import {
   Chart as ChartJS,
   RadialLinearScale,
@@ -7,10 +7,9 @@ import {
   Filler,
   Tooltip,
   Legend,
-} from 'chart.js';
-import { Radar } from 'react-chartjs-2';
-import reactLogo from './assets/react.svg'
-import './App.css'
+} from "chart.js";
+import { Radar } from "react-chartjs-2";
+import "./App.scss";
 
 ChartJS.register(
   RadialLinearScale,
@@ -22,35 +21,35 @@ ChartJS.register(
 );
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [champsInfo, setChampsInfo] = useState <any>({});
-  const [champ, setChamp] = useState <any>({
+  const [champsInfo, setChampsInfo] = useState<any>({});
+  const [champ, setChamp] = useState<any>({
     name: "Aatrox",
     info: {
-        "attack": 8,
-        "defense": 4,
-        "magic": 3,
-        "difficulty": 4
+      attack: 8,
+      defense: 4,
+      magic: 3,
+      difficulty: 4,
     },
     image: {
-        "full": "Aatrox.png",
-        "sprite": "champion0.png",
-        "group": "champion",
-        "x": 0,
-        "y": 0,
-        "w": 48,
-        "h": 48
-    }
+      full: "Aatrox.png",
+      sprite: "champion0.png",
+      group: "champion",
+      x: 0,
+      y: 0,
+      w: 48,
+      h: 48,
+    },
   });
 
-
   const statsToParse = Object.entries(champ?.info) || [];
-  const labelInfo = statsToParse?.map((stat) => {
-    return stat[0];
-  }) || [];
-  const statDataInfo = statsToParse?.map((stat) => {
-    return stat[1];
-  }) || [];
+  const labelInfo =
+    statsToParse?.map((stat) => {
+      return stat[0];
+    }) || [];
+  const statDataInfo =
+    statsToParse?.map((stat) => {
+      return stat[1];
+    }) || [];
 
   const data = {
     labels: labelInfo || [],
@@ -58,8 +57,8 @@ function App() {
       {
         label: `${champ.name} stats`,
         data: statDataInfo,
-        backgroundColor: 'rgba(255, 99, 132, 0.2)',
-        borderColor: 'rgba(255, 99, 132, 1)',
+        backgroundColor: "rgba(255, 99, 132, 0.2)",
+        borderColor: "rgba(255, 99, 132, 1)",
         borderWidth: 1,
       },
     ],
@@ -67,47 +66,63 @@ function App() {
 
   useEffect(() => {
     async function getChamps() {
-      const res = await fetch('https://ddragon.leagueoflegends.com/cdn/13.1.1/data/en_US/champion.json');
+      const res = await fetch(
+        "https://ddragon.leagueoflegends.com/cdn/13.1.1/data/en_US/champion.json"
+      );
       const parsedRes = await res.json();
-      console.log(parsedRes.data);
       setChampsInfo(parsedRes.data as any);
     }
     getChamps();
-    return(()=> {
+    return () => {
       setChampsInfo([]);
-    })
-  }, [])
-  
-  async function getChampName(name: string) {
-     const res = await fetch(`https://ddragon.leagueoflegends.com/cdn/13.1.1/img/champion/${name}.png`);
-  }
- 
-  console.log(champ);
+    };
+  }, []);
 
   return (
     <div className="App">
       <h1>LoL Sapiens</h1>
-      {champsInfo &&
-        <>
-          <select onChange={(e) => {setChamp(champsInfo[e.target.value])}}>
-            {Object.keys(champsInfo).map((champ: any) => {
-              return (
-                <option value={champ} key={champ}>
-                  {champ}
-                </option>
-              )
-            })}
+
+      {champsInfo && (
+        <select
+          onChange={(e) => {
+            setChamp(champsInfo[e.target.value]);
+          }}
+        >
+          {Object.keys(champsInfo).map((champ: any) => {
+            return (
+              <option value={champ} key={champ}>
+                {champ}
+              </option>
+            );
+          })}
         </select>
-        </>
-      }
-      <>
-      {champ.image &&
-        <img src={`https://ddragon.leagueoflegends.com/cdn/13.1.1/img/champion/${champ.image?.full || ''}`}/>
-      }
-      </>
-      <Radar data={data} />
+      )}
+
+      <section className="Champion bg__gray">
+        {champ.image && (
+          <img
+            src={`https://ddragon.leagueoflegends.com/cdn/13.1.1/img/champion/${
+              champ.image?.full || ""
+            }`}
+          />
+        )}
+
+        <div className="Champion__info">
+          <h2>{champ.name}</h2>
+          <div className="Tags">
+            {champ.tags.map((tag: string) => (
+              <span className="Chip" key={tag}>
+                {tag}
+              </span>
+            ))}
+          </div>
+        </div>
+        <div className="Champion__stats">
+          <Radar data={data} />
+        </div>
+      </section>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
