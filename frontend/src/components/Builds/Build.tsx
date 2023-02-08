fb0e1a3d391ba10288e952ca942d12e0b517a84c
Andres Gonzalez <adgonzalezo@unal.edu.co>
2023-02-08 12:49:38 -0500

    AAAA que no se puede subir una carpeta con nombre Build

---
+++ frontend/src/components/Builds/Build.tsx
@@ -0,0 +1,63 @@
+import { useEffect, useState } from "react";
+import "./build.scss";
+
+interface BuildProps {
+  champ: any;
+  lane: string;
+  tier: string;
+  mode: string;
+  spicy: string;
+}
+
+const Build = ({ champ, lane, tier, mode, spicy }: BuildProps) => {
+  const [build, setBuild] = useState<any>(null);
+
+  const getBuild = async () => {
+    const res = await fetch(
+      "http://localhost:3200/build?" +
+        new URLSearchParams({
+          champion_id: champ?.key,
+          lane,
+          tier,
+          mode,
+          keystone_id: "8128",
+          spicy,
+        })
+    );
+
+    const parsedRes = await res.json();
+    setBuild(parsedRes);
+    console.log(parsedRes);
+  };
+
+  useEffect(() => {
+    if (champ && tier && lane && mode && spicy) getBuild();
+  }, [champ, tier, lane, mode, spicy]);
+
+  return (
+    build && (
+      <section className="card bg__gray">
+        <h2>{build.title}</h2>
+        <div className="build">
+          {build.blocks.map((block: any) => (
+            <div key={block.type} className="build-block">
+              <p>{block.type}</p>
+              <div className="build-block__items">
+                {block.items.map((item: any, index: number) => (
+                  // Se usa el index porque Sebastian manda items repetidos :/
+                  <img
+                    key={`${item.id}-${index}`}
+                    src={`http://ddragon.leagueoflegends.com/cdn/13.1.1/img/item/${item.id}.png`}
+                    alt={`item-${item.id}`}
+                  />
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+    )
+  );
+};
+
+export default Build;
--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -3,6 +3,6 @@ import Select from "./Select/Select";
 import ChampionCard from "./ChampionCard/ChampionCard";
 import Tooltip from "./Tooltip";
 import Portal from "./Portal";
-import Build from "./Build/Build";
+import Build from "./Builds/Build";
 
 export { Bans, Select, ChampionCard, Tooltip, Portal, Build };
