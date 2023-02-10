6b64bc5baeff919a461e52eb0acc5756114a9d97
Daniel Castañeda <dfcastanedat@unal.edu.co>
2023-02-09 21:46:40 -0500

    hotfix 1

--- frontend/src/pages/InfoAndBans/InfoAndBans.tsx
+++ frontend/src/pages/InfoAndBans/InfoAndBans.tsx
@@ -49,7 +49,7 @@ function InfoAndBans() {
         <Select
           itemList={championList}
           onChangeCallback={getChampInfo}
-          defaultValue={champ.name || "Aatrox"}
+          defaultValue={champ?.name || "Aatrox"}
         />
         <Select
           itemList={lanes}
