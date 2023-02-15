7ae49176283b4949cc01e5e07a8fa8a94cc8af9f
Juan Sebastian Peña <36420972+jspenaq@users.noreply.github.com>
2023-02-15 13:20:09 -0500

    updating main.yml: added coverage to backend tests (#25)
    * updating main.yml: added coverage to backend tests
    
    * updating main.yml: added html coverage
    
    * deleted lcu folder: check frontend
    
    * updated coverage backend
    
    * linted
    
    * added date
    
    * updated coverage

---
+++ backend/tests/lolsapiens_constants_test.py
@@ -0,0 +1,10 @@
+from backend.api.constants import champion_id_param
+from fastapi import Query
+
+
+class TestConstants:
+    def test_champion_id_param(self):
+        value = champion_id_param
+        assert value.title == "champion_id"
+        assert value.description != ""
+        assert value.example is not None
