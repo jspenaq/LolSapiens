fc8c84649f38e424698b34f06e08f5801cffc65f
Juan Sebastian Peña <jspenaq@unal.edu.co>
2023-03-09 10:47:52 -0500

    fixed Precision bug

--- frontend/src/constants/runesPath.ts
+++ frontend/src/constants/runesPath.ts
@@ -1,7 +1,7 @@
 export const RunesPath = new Map([
   ["Domination", 8100],
   ["Inspiration", 8300],
-  ["Precision", 8800],
+  ["Precision", 8000],
   ["Resolve", 8400],
   ["Sorcery", 8200],
 ]);
