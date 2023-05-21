d88f73116b7d94f33ef0cbb35dc687109e0bf45f
Juan Sebastian Peña <jspenaq@unal.edu.co>
2023-05-21 01:10:39 -0500

    refactor get top bans - now has limit value

--- backend/api/router/bans.py
+++ backend/api/router/bans.py
@@ -1,10 +1,14 @@
 from fastapi import APIRouter, Query
-from backend.api.constants import s, lane_param, tier_param
+from backend.api.constants import s, lane_param, tier_param, limit_param
 
 
 bans_router = APIRouter()
 
 
 @bans_router.get("/bans/top10", tags=["tierlist"])
-def get_top10_bans(lane: str = lane_param, tier: str = tier_param):
-    return s.get_top10_bans(lane, tier)
+def get_top_bans(
+    lane: str = lane_param,
+    tier: str = tier_param,
+    limit: int = limit_param,
+):
+    return s.get_top_bans(lane, tier, limit)
