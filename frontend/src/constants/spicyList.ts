76bb24a07a031965771170b2940000e0aee1972d
Juan Sebastian Peña <36420972+jspenaq@users.noreply.github.com>
2023-08-03 16:23:41 -0500

    Main to prod (#38)
    * feat: Update base URL in Sapiens API
    
    * feat: Add emerald_plus tier to tiers constant
    
    * refactor: Format code
    
    * feat: Add "arena" mode to rawModes array
    
    * feat: Add support for "arena" mode
    
    * feat: Add "10" to rawSpicy array
    
    * refactor: Comment out the condition to limit the number of items
    
    * fix: Update the branch name in the on trigger for the release workflow
    
    * refactor: Remove unused imports in utils.py

--- frontend/src/constants/spicyList.ts
+++ frontend/src/constants/spicyList.ts
@@ -1,6 +1,6 @@
 import { parseSelectValues } from "../utils";
 
-const rawSpicy = ["0", "1", "2", "3", "4"];
+const rawSpicy = ["0", "1", "2", "3", "4", "10"];
 
 export default rawSpicy.map((spicy) => {
   return {
